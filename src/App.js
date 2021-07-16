b9ec8a7c01bdd210c2155abfa744e3ba1a072115
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-16 13:07:21 -0400

    added redux packages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/products')
-      .then(resp => resp.json())
-      .then(data => console.log(data))
-  }
-
   render() {
   return (
     <div className="App">
