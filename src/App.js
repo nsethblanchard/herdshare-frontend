f9c73905d970722e4ff07e83c2363bad8875d868
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-21 16:40:11 -0400

    adding some notes to App.js

--- src/App.js
+++ src/App.js
@@ -19,5 +19,24 @@ class App extends React.Component {
   }
 }
 
+// way of accessing or seeing our state in store and directing which particular pieces we want- passed to the connect as first argument
 
+const mapStateToProps = (state) => {
+  return {
+    carts: state.accounts
+  }
+}
+
+// used as second argument and sends (dispatches) the necessary action to update our state in redux
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+}
+
+
+// using this action creator, we can skip the mapDispatchToProps function and just directly pass in the action creator and update store
 export default connect(null, {fetchCarts})(App);
+
+// if you are only updating store and don't need the initial state information, you can use "null" as the first argument
+// gives us access to this.props.fetchCarts()
