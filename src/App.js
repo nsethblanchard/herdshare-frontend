2163a3b5677aefd2be0422b619b45edf6a6de33d
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-22 09:23:28 -0400

    creating all cart components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import {connect} from 'react-redux'
 import { fetchCarts } from './actions/fetchCarts';
+import CartsContainer from './containers/CartsContainer';
 
 class App extends React.Component {
 
   render() {
   return (
     <div className="App">
-      Put App Brilliance Here
+      <CartsContainer />
+      
     </div>
   );
   }
--- src/containers/CartsContainer.js
+++ src/containers/CartsContainer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-class cartsContainer extends Component {
+class CartsContainer extends Component {
     render() {
         return (
             <div>
-                
+                Cart Container
             </div>
         )
     }
 }
 
-export default cartsContainer
+export default CartsContainer;
