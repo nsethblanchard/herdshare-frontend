c9d0bcc3c50a2228daaa13a611fe5e2fa23f6034
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-31 13:13:33 -0400

    moved around some imports and started Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import Nav from './components/layout/Nav';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
 import FarmersContainer from './containers/FarmersContainer'
 import CustomersContainer from './containers/CustomersContainer'
 import ProductsContainer from './containers/ProductsContainer';
+import ProductsList from './components/ProductsList';
 
 
 
 class App extends React.Component {
   render() {
     return (
-    <div className="App">
-      <CustomersContainer />
-      <FarmersContainer />
-      <ProductsContainer />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+          Here we will put "Welcome" and then maybe map through all farmers and have a user choose who they are...
+          <Switch>
+            <Route path='/products' component={ProductsList} />
+
+          </Switch>        
+      </div>
+    </Router>
     );
   }
 }
---
+++ src/components/layout/Nav.js
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom';
+
+function Nav() {
+    const navStyle = {
+        color: 'white'
+    };
+
+    return (
+        <nav>
+            <h2>Welcome to Herd Share</h2>
+            <ul className="nav-links">
+                <Link style={navStyle} to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link style={navStyle} to='/products'>
+                    <li>All Products</li>
+                </Link>
+                <Link style={navStyle} to='/customers'>
+                    <li>Available Customers</li>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
