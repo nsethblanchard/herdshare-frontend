4784dc383a11f5ab148501acb5f777be8a737af1
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-02 08:11:08 -0400

    changing flow around

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './Nav';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Home from './Home';
 import ProductsList from './components/ProductsList';
 import CustomersList from './components/CustomersList';
 
@@ -15,6 +16,7 @@ class App extends React.Component {
         <Nav />
           
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route exact path='/customers' component={CustomersList} />
             <Route exact path='/products' component={ProductsList} />
 
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FarmersContainer from './containers/FarmersContainer'
+
+
+function Home() {
+    
+
+    return (
+        <div>
+            <h1>Home Screen</h1>
+            <FarmersContainer />
+
+        </div>
+    );
+}
+
+export default Home;
